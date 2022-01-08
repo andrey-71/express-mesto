c33391923ce252d3a203a6c4d778845dd1949e46
andrey-71 <and_go1991@mail.ru>
2022-01-08 23:02:11 +0300

    feat: добавить модель пользователя в контроллер

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const {PORT = 3000} = process.env;
 const app = express();
@@ -12,11 +12,9 @@ app.use(routes);
 
 
 // Подключение к БД
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// });
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true
+});
 
 
 app.listen(PORT, () => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,35 @@
-const test = require('../test.json'); //del
+const User = require('../models/user');
 
 const getUsers = (req, res) => {
-  res.send(test);
-}
+  return User.find({})
+    .then(users => res.status(200).send(users))
+    .catch(() => res.status(500).send({message: 'На сервере произошла ошибка'}));
+};
 
 const getUser = (req, res) => {
-  const { id } = req.params;
-  const user = test.find(u => u.id === id);
-
-  res.send(user);
-}
+  return User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({message: 'Пользователь по указанному _id не найден'});
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+};
 
 const createUser = (req, res) => {
-  res.status(200).send(req.body);
-}
+  return User.create({...req.body})
+    .then(user => res.status(201).send(user))
+    .catch(err => {
+      console.log(err);
+    })
+};
 
 module.exports = {
   getUsers,
   getUser,
   createUser
-}
+};
