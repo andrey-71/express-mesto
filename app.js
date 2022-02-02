57a5b036133c75e2c9c5bf2700f93fd060a6cd7a
andrey-71 <and_go1991@mail.ru>
2022-02-02 17:05:57 +0300

    feat: реализовать мидлвэр авторизации, подключить его до защищенных роутов

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const { PORT, DB_ADDRESS } = require('./utils/config');
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const app = express();
 // Сборка данных в JSON-формат
@@ -14,14 +15,16 @@ app.use(bodyParser.json());
 app.post('/signup', createUser);
 app.post('/signin', login);
 
-// Мидлвэр для временного решения хранения id пользователя
-app.use((req, res, next) => {
-  req.user = {
-    _id: '61dea19b6226dd51fcc15da9',
-  };
+app.use(auth);
 
-  next();
-});
+// Мидлвэр для временного решения хранения id пользователя
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '61dea19b6226dd51fcc15da9',
+//   };
+//
+//   next();
+// });
 
 // Остальные роуты (защищенные)
 app.use(routes);
