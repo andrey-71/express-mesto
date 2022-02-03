51611cbd1dc93a4b63af22acb86e9a8cc9385d4b
andrey-71 <and_go1991@mail.ru>
2022-02-03 12:38:15 +0300

    refactor: доработать контроллер карточки в части удаления авторизованным пользователем только собственных карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,5 @@
 const Card = require('../models/card');
-const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../utils/errors');
+const { BAD_REQUEST, FORBIDDEN, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../utils/errors');
 
 // Получение всех карточек
 module.exports.getCards = (req, res) => Card.find({})
@@ -28,22 +28,32 @@ module.exports.createCard = (req, res) => Card.create({
   });
 
 // Удаление карточки
-module.exports.deleteCard = (req, res) => Card.findByIdAndRemove(req.params.id)
-  .orFail(() => {
-    throw new Error('NotFoundError');
-  })
-  .then((card) => {
-    res.status(200).send(card);
-  })
-  .catch((err) => {
-    if (err.message === 'NotFoundError') {
-      res.status(NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
-    } else if (err.name === 'CastError') {
-      res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные при удалении карточки' });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
-    }
-  });
+module.exports.deleteCard = (req, res) => {
+  return Card.findById(req.params.id)
+    .orFail(() => {
+      throw new Error('NotFoundError');
+    })
+    .then((card) => {
+      if(card.owner.toString() === req.user._id) {
+        Card.findByIdAndRemove(req.params.id)
+          .then(() => res.status(200).send(card));
+      } else {
+        throw new Error('ForBiddenError');
+      }
+    })
+    .catch((err) => {
+      if (err.message === 'NotFoundError') {
+        res.status(NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+      } else if (err.message === 'ForBiddenError') {
+        res.status(FORBIDDEN).send({ message: 'Вы можете удалить только свою карточку' });
+      }
+      else if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные при удалении карточки' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+}
 
 // Постановка лайка
 module.exports.likeCard = (req, res) => {
