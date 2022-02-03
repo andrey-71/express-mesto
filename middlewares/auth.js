db3a5252d431c2fb505e3a755271ecaa9fe12454
andrey-71 <and_go1991@mail.ru>
2022-02-03 18:29:52 +0300

    feat: создать остальные классы ошибок

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/config');
-const UnauthorizedError = require('../errors/Unauthorized');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
